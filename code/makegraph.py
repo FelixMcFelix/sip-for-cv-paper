a17f9ed9b67fa196ce8e3f1f8eb2702928320e08
Kyle Simpson <kyleandrew.simpson@gmail.com>
2017-01-17 03:44:08 +0000

    [MakeGraph] Slight reorganisation

--- code/makegraph.py
+++ code/makegraph.py
@@ -9,24 +9,34 @@ import skimage.morphology as morph
 import matplotlib.pyplot as plt
 
 def main(input, output, plot):
+	##
 	# Step 1: read input image
+	##
 	image = color.rgb2grey(data.imread(input))
 	show(plot, image)
 
+	##
 	# Step 2: threshold, and invert to make text "foreground"
+	##
 	thres = np.invert(image > filters.threshold_li(image))
 	show(plot, thres)
 
+	##
 	# Step 3: skeletonize
+	##
 	skel = morph.skeletonize(thres)
 	show(plot, skel)
 
+	##
 	# Step 4: connected component analysis
+	##
 	cc = morph.label(skel)
 	n_classes = np.max(cc)
 	show(plot, cc)
 
+	##
 	# Step 5: endpoints, intersections, keypoints
+	##
 	endpoint_sets = [[] for i in xrange(n_classes)]
 	inters = []
 
@@ -52,8 +62,13 @@ def main(input, output, plot):
 
 	print endpoint_sets, inters
 
+	##
 	# Step 6: Line/Path detection
+	##
+
+	##
 	# Step 7: North and Component connection
+	##
 
 def get_nhood(image, x, y):
 	nhood = np.zeros((3,3), dtype=np.int8)
@@ -72,9 +87,6 @@ def fetch(image, x, y, val=-1):
 		return val
 	else:
 		return image[y, x]
-
-def invert_binary(img):
-	return (img + 1) % 2
 
 def show(trigger, img, grey=True):
 	if trigger:
